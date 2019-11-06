cb06e90ee7c9a4b39ce981fe7b9107bd3c8b90ad
Gregg Keithley <gregg@pronym.com>
2019-11-06 09:40:35 -0500

    Initial commit.

---
+++ greenkey/conf/environments/production.py
@@ -0,0 +1,5 @@
+from greenkey.conf.generic.settings import *  # noqa
+
+
+DEBUG = False
+DEBUG_STATIC_FILES = False
---
+++ greenkey/conf/environments/vagrant.py
@@ -0,0 +1,4 @@
+from greenkey.conf.generic.settings import *  # noqa
+
+DEBUG = True
+DEBUG_STATIC_FILES = True
